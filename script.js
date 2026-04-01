5260981677245ae0878d230064d7aded109d5c65
Pravin Rathod <medicogayatri3@gmail.com>
2026-04-01 17:39:09 +0530

    a scrambling text effect

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-addEventListener("mousemove",(e)=>{
-    document.body.style.setProperty("--x",e.clientX+"px")
-    document.body.style.setProperty("--y",e.clientY+"px")
-})
+const para = document.querySelector("p");
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const originalText = para.innerText;
+let timer = null;
+
+para.addEventListener("mouseenter", () => {
+    clearInterval(timer);
+
+    timer = setInterval(() => {
+        const scrambled = originalText.split('').map(() => {
+            const rl = characters[Math.floor(Math.random() * characters.length)];
+            return rl;
+        }).join('');
+
+        para.innerText = scrambled;
+    }, 100);
+});
+
+
+para.addEventListener("mouseleave", () => {
+    clearInterval(timer);
+    para.innerText = originalText;
+});
